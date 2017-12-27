255f5026b876eef99b18e11ae573a7e47ac549d0
Meaghan <meaghan.m.jones@gmail.com>
2017-12-27 14:57:46 +0100

    create first react component

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,52 +1,13 @@
-const app = {
-  title: 'Best App Ever',
-  subtitle: 'Meaghan does make the best apps',
-  options: []
-};
-
-const onFormSubmit = (e) =>  {
-  e.preventDefault();
-  const option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    render();
+class Header extends React.Component {
+  render () {
+    return <p>This is from Header</p>;
   }
-};
-
-const removeAll = () => {
-  app.options = [];
-  render();
 }
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-}
-const appRoot = document.getElementById('app');
+const jsx = (
+  <div>
+    <Header />
+  </div>
+);
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options.'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-      <ol>
-        {
-          app.options.map((option) => <li key={option}>{option}</li>)
-        }
-      </ol>
-      <button onClick={removeAll}>Remove Options</button>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(jsx, document.getElementById('app'));
--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -15,8 +15,47 @@ class Person {
 
 }
 
-const me = new Person('Meaghan Jones', 32);
-console.log(me.getDescription());
+class Student extends Person {
+  constructor(name, age, major){
+    super(name, age);//calling parent constructor function and passing them through
+    this.major = major;
+  }
+  hasMajor(){
+    return !!this.major;
+  }
+  getDescription(){
+    let description = super.getDescription();
+    if (this.hasMajor) {
+      description = description += ` Their major is ${this.major}.`
+    }
+    return description;
+  }
 
-const other = new Person ();
-console.log(other.getDescription());
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation){
+    super(name, age);//calling parent constructor function and passing them through
+    this.homeLocation = homeLocation;
+  }
+  hasHomeLocation(){
+    return !!this.homeLocation;
+  }
+  getGreeting(){
+    let greeting = super.getGreeting();
+    if (this.hasHomeLocation) {
+      greeting = greeting += `. I'm visiting from ${this.homeLocation}.`
+    }
+    return greeting;
+  }
+
+}
+
+
+const me = new Traveler ('Sonja', 29, 'Portland');
+console.log(me.getGreeting());
+// const me = new Student('Meaghan Jones', 32, 'Political Science');
+// console.log(me.getDescription());
+//
+// const other = new Student ();
+// console.log(other.getDescription());
